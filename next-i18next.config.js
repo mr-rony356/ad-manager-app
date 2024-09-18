3482594684fa35ecf625e00d03bd9b2a037f1252
Omor Faruk Rony <omorfarukrony2@gmail.com>
2024-09-18 09:12:26 +0600

    Next Config add for Translation

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,4 +3,9 @@ module.exports = {
     defaultLocale: "de",
     locales: ["de", "en"],
   },
+  localePath:
+    typeof window === "undefined"
+      ? require("path").resolve("./public/locales")
+      : "/locales",
+  reloadOnPrerender: process.env.NODE_ENV === "development",
 };
