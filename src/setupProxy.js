ed1383fd8ccb737d59a774c0465069a5d2a3f739
Omor Faruk Rony <omorfarukrony2@gmail.com>
2024-10-30 02:47:08 +0600

    onlyfriend.live api adds

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
     createProxyMiddleware({
       target:
         process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
-          ? "https://onlyfriend.ch"
+          ? "https://onlyfriend.live"
           : "http://localhost:3000/",
       changeOrigin: true,
     }),
